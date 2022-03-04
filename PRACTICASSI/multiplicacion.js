45e8521e391f029bee313811502df2203dabe5a9
Geinly Sepúlveda <100728861+geinly99@users.noreply.github.com>
2022-03-03 20:32:22 -0700

    Add files via upload

---
+++ PRACTICASSI/multiplicacion.js
@@ -0,0 +1,9 @@
+for(let i=1; i<=10; i++){
+    for (let j=1; j<=10; j++){
+        console.log(`${i} x ${j} = ${i*j}`);
+    }
+}
+/*
+el primer for hace iteración cada vez que se cumplan las iteraciones del segundo for 
+*/
+
