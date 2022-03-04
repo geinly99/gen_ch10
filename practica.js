be744de7a97ebffea76279b94190b311d1a971ac
geinly99 <geinly99@gmail.com>
2022-03-03 20:53:34 -0700

    Created PRACTICASSI

---
+++ practica.js
@@ -0,0 +1,18 @@
+let perro = "zeus";
+console.log(typeof(perro));
+console.log("HOLA");
+function saludar() {
+    console.log("HOLA ¿CÓMO ESTÁS?");
+}
+saludar();
+function saludarNombre(nombre){
+console.log("Hola" + nombre);
+}
+saludarNombre("Gisel");
+function retornarDatos(numero){
+let resultado = numero + 10;
+return resultado;
+}
+
+retornarDatos(20);
+console.log(retornarDatos(20));
